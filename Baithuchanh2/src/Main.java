5e10c1af34d557b26f87f14a3faf267fb0d300c1
Hung <hung18061999hung@gmail.com>
2022-10-12 23:37:39 +0700

    Baithuchanh 2-Bai1

---
+++ Baithuchanh2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        float f = 20.5F;
+        double d = 20.5;
+        boolean b =true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("a = "+a);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("s = "+s);
+    }
+}
